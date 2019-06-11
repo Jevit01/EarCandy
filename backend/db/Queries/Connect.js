c439badc658260b25b557f3e0a6f4546f466cc9e
Thomas Perez <thomasperez@pursuit.org>
2019-06-11 15:51:18 -0400

    test

--- backend/db/Queries/Connect.js
+++ backend/db/Queries/Connect.js
@@ -1,5 +1,8 @@
 const pgp = require("pg-promise")({});
-var connectionString = process.env.DATABASE_URL;
+var connectionString =
+  process.env.DATABASE_URL || "postgres://localhost/earworm";
 const db = pgp(connectionString);
+
+console.log(process.env);
 
 module.exports = { db };
