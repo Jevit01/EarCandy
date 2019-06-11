f92cf996ec90fffd7a3694d58429eb0957e0acdb
Thomas Perez <thomasperez@pursuit.org>
2019-06-11 16:34:18 -0400

    test1

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
