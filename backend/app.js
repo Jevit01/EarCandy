cee3184dd35b9960f89e4e5dd7ea8c8c09082297
Thomas Perez <thomasperez@pursuit.org>
2019-06-11 16:43:45 -0400

    test

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
 });
 
 // error handler
